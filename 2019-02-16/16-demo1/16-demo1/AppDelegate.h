d6af6a373f0b43c1881bf7ae3ca94394218b887f
wax2045 <861412028@qq.com>
2019-02-16 22:22:20 +0800

    init

---
+++ 2019-02-16/16-demo1/16-demo1/AppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.h
+//  16-demo1
+//
+//  Created by mac on 2019/2/16.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+/*
+ Core Animation 一个令人误解的命名 复合引擎
+ Layout Kit ？？
+ 内容是被分解成独立的图层
+ 图层树体系中
+ 基础
+ 
+ iOS中，所有的试图都从UIView的基类派生而来
+ 
+ 
+ */
